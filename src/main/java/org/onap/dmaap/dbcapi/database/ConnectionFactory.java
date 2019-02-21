95321953d39e368a5e89344ca220ec7dd479afe1
Driptaroop Das <driptaroop.das@in.ibm.com>
2019-02-22 00:54:32 +0530

    Sonar Fix - ConnectionFactory.java
    Sonar Fix - ConnectionFactory.java
    
    Issue-ID: DMAAP-1056
    Change-Id: I1701b0ab9b26513d7ead3bb25b3436105646f18c
    Signed-off-by: Driptaroop Das <driptaroop.das@in.ibm.com>

--- src/main/java/org/onap/dmaap/dbcapi/database/ConnectionFactory.java
+++ src/main/java/org/onap/dmaap/dbcapi/database/ConnectionFactory.java
@@ -3,6 +3,7 @@
  * org.onap.dmaap
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2019 IBM.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,6 +109,7 @@ public class ConnectionFactory	{
 			TimeUnit.SECONDS.sleep(seconds);
 		} catch (InterruptedException e) {
 			logger.debug("Waiting interrupted. ", e);
+			Thread.currentThread().interrupt();
 		}
 	}
 }
