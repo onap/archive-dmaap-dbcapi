71c4d3a102c962332ecf8e1d6006681c507dd9f0
pkaras <piotr.karas@nokia.com>
2019-04-16 12:02:17 +0200

    AllowAll api policy added
    Change-Id: I1673b5647b0917ca18fe9b814d876a07d881021a
    Issue-ID: DMAAP-1166
    Signed-off-by: piotr.karas <piotr.karas@nokia.com>

--- src/main/java/org/onap/dmaap/dbcapi/authentication/ApiPolicy.java
+++ src/main/java/org/onap/dmaap/dbcapi/authentication/ApiPolicy.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,42 +24,40 @@ import org.onap.dmaap.dbcapi.logging.BaseLoggingClass;
 import org.onap.dmaap.dbcapi.logging.DmaapbcLogMessageEnum;
 import org.onap.dmaap.dbcapi.util.DmaapConfig;
 
-public class ApiPolicy extends BaseLoggingClass {
-	static String allow = "allow";
-	String dClass = null;
-	private boolean useAuthClass;
-	ApiAuthorizationCheckInterface perm = null;
-	
-	public ApiPolicy() {
-		DmaapConfig p = (DmaapConfig)DmaapConfig.getConfig();
-		dClass = p.getProperty( "ApiPermission.Class", allow );
-		logger.info( "ApiPolicy implements " + dClass);
-		if ( dClass.equalsIgnoreCase( allow )) {
-			useAuthClass = false;
-			return;
-		} 		
-		useAuthClass = true;
-		logger.info( "dClass=" + dClass + " useAuthClass=" + useAuthClass );
-		try {
-			perm = (ApiAuthorizationCheckInterface) (Class.forName(dClass).newInstance());	
-		} catch (Exception ee ) {
-			errorLogger.error(DmaapbcLogMessageEnum.UNEXPECTED_CONDITION, "attempting to instantiate " + dClass  );		
-			errorLogger.error( "trace is: " + ee );
-		}	
-	}
-	
-	public void check( String mechid, String pwd, DmaapPerm p ) throws AuthenticationErrorException {
-		if ( dClass.equalsIgnoreCase( allow )) {
-			return;
-		}
-		
-		// execute check of loaded class
-		perm.check( mechid, pwd, p );
-	
-	}
-	
-	public boolean getUseAuthClass() {
-		return useAuthClass;
-	}
+import java.util.Properties;
 
+public class ApiPolicy extends BaseLoggingClass {
+
+    private boolean useAuthClass = true;
+    private ApiAuthorizationCheckInterface perm = null;
+
+    public ApiPolicy() {
+        this(DmaapConfig.getConfig());
+    }
+
+    ApiPolicy(Properties p) {
+        String dClass = p.getProperty("ApiPermission.Class");
+        logger.info("ApiPolicy implements " + dClass);
+        logger.info("dClass=" + dClass + " useAuthClass=" + useAuthClass);
+
+        try {
+            perm = (ApiAuthorizationCheckInterface) (Class.forName(dClass).newInstance());
+        } catch (Exception ee) {
+            errorLogger.error(DmaapbcLogMessageEnum.UNEXPECTED_CONDITION, "attempting to instantiate " + dClass);
+            errorLogger.error("trace is: " + ee);
+            useAuthClass = false;
+        }
+    }
+
+    public void check(String mechid, String pwd, DmaapPerm p) throws AuthenticationErrorException {
+        perm.check(mechid, pwd, p);
+    }
+
+    public boolean getUseAuthClass() {
+        return useAuthClass;
+    }
+
+    ApiAuthorizationCheckInterface getPerm() {
+        return perm;
+    }
 }
