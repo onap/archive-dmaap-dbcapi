e90fb6b054e978da7a1b41da0a87df1482b5d42f
ajay_dp001 <ajay.deep.singh@est.tech>
2021-01-23 14:50:45 +0530

    [DMaap-dbcapi] Updated Outdated Packages
       - Log4j Version 1.2.17 to 2.13.3
       - commons-codec version 1.11 to 1.15
       - postgresql version 42.2.5 to 42.2.18
    
    Issue-ID: DMAAP-1515
    Signed-off-by: ajay_dp001 <ajay.deep.singh@est.tech>
    Change-Id: I1716c8c78d4c3ca4f75159b3de98d9c799bae929

--- src/test/java/org/onap/dmaap/dbcapi/model/TestRunner.java
+++ src/test/java/org/onap/dmaap/dbcapi/model/TestRunner.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,19 +18,24 @@
  * ============LICENSE_END=========================================================
  */
 package org.onap.dmaap.dbcapi.model;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-import org.apache.log4j.Logger;
+
 public class TestRunner {
-	private static final Logger LOGGER = Logger.getLogger(TestRunner.class);
-	public static void main(String[] args) {
-		
-		Result result = JUnitCore.runClasses(JUnitTestSuite.class);
-		for (Failure failure : result.getFailures()) {
-			LOGGER.info(failure.toString());
-			
-		}
-		LOGGER.info(result.wasSuccessful());
-	}
+
+    private static final Logger logger = LogManager.getLogger(TestRunner.class);
+
+    public static void main(String[] args) {
+
+        Result result = JUnitCore.runClasses(JUnitTestSuite.class);
+        for (Failure failure : result.getFailures()) {
+            logger.info(failure.toString());
+
+        }
+        logger.info(result.wasSuccessful());
+    }
 }
